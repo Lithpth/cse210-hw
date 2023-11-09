bfb3e21d81eb9b5b00d9f9dc68e727dee104e0fe
Lithpth <disasterpower6141@gmail.com>
2023-11-08 17:17:05 -0700

    Prep1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+
+        // Good practice to list variables first
+        string firstName, lastName;
+
+        Console.Write("What is your first name? ");
+        firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        lastName = Console.ReadLine();
+        
+        Console.WriteLine();
+
+        Console.WriteLine("Your name is {1}, {0} {1}.",firstName,lastName);
+
+
+        Console.ReadLine();
+
     }
 }
