907e4bdbfc52c81d0a1febcc828ee2647db96956
Lithpth <disasterpower6141@gmail.com>
2023-11-11 21:40:59 -0700

    Prep 3 and 4 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        int num1,guess=0,n=0;
+
+        int guesses=0;
+
+        Console.Write("What is the magic number? ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        // Console.Write("what is your guess? ");
+        
+        // guess = Convert.ToInt32(Console.ReadLine());
+
+        while (guess!=num1) // While guess does not equal num1, keep running
+        {
+            Console.Write("what is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            guesses+=1; // The number of guesses is incremented by one
+
+            //if else portion
+            if (num1>guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (num1<guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+                break;            
+            }
+        }
+        Console.WriteLine("Good job! You guessed it in {0} attempts", guesses);
+        Console.ReadLine();
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,87 @@
 using System;
-
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+        // // List<int> numbers;
+        // // numbers = new List<int>();
+        // // List<string> words;
+        // // words = new List<string>();
+
+        // // Usually done on same line
+        // // Parentheses used any time we need to create a new object
+        // // List<int> numbers = new List<int>();
+        // List<string> words = new List<string>();
+
+        // // Now we gonna add strings to the list using the .Add() method
+        // words.Add("phone");
+        // words.Add("keyboard");
+        // words.Add("mouse");
+        
+
+        // Console.WriteLine(words.Count); // Count gets the size of list
+
+        // // Now time to iterate through a list
+
+        // // foreach
+        // foreach (string word in words)
+        // {
+        //     Console.WriteLine(word);
+        // }
+        // // for loop iteration
+        // for (
+        //     int i=0; // loop variable initialization
+        //     i < words.Count; // condition
+        //     i++ // increment
+        // )
+        // {
+
+        //     Console.WriteLine(words[i]);
+        // }
+
+        List<double> numbers = new List<double>();
+
+        double number = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {
+            
+            Console.Write("Enter number: ");
+            number= Double.Parse(Console.ReadLine());
+            if ((number) != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        // find sum
+        double sum = 0;
+        for (int i=0; i < numbers.Count; i++)
+        {
+            sum += numbers[i];
+        }
+
+        // find average
+        double avg = (double)sum/numbers.Count;
+        
+        // find max number in list
+        double max = double.MinValue;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > max)
+                max = numbers[i];
+        }
+
+        // Display
+        Console.WriteLine("The sum is: {0}",sum);
+        Console.WriteLine("The average is: {0}",avg);
+        Console.WriteLine("The largest number is: {0}",max);
+
+
+        Console.ReadLine();
     }
 }
