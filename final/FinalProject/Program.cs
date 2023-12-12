7644c132265a8d30f8994518875896bb0f976021
Lithpth <disasterpower6141@gmail.com>
2023-12-12 14:49:25 -0700

    Lesson 5 completion and final project milestone

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,147 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+
+    public class Program
     {
-        Console.WriteLine("Hello FinalProject World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            MarketBuy marketBuy = new MarketBuy("r",11.14f,140f,"buy");
+
+            // Temporary
+            marketBuy.GetOrder(150);
+            Console.ReadLine();
+        }
     }
-}
+    public abstract class Order
+    {
+        private int _id;
+        private string _assetName;
+        private string _orderType;
+        public float _balance;
+
+        public Order(string assetName, string orderType, int id, float balance = 143.5f)
+        {
+            _assetName = assetName;
+            _orderType = orderType;
+            _balance = balance;
+
+        }
+
+        protected Order(string assetName)
+        {
+        }
+
+        // Id
+        public int GetId()
+        {
+            return _id;
+        }
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+        // Name
+        public string GetAssetName()
+        {
+            return _assetName;
+        }
+        public void SetAssetName(string assetName)
+        {
+            _assetName = assetName;
+        }
+        public string GetOrderType()
+        {
+            return _orderType;
+        }
+        public void SetOrderType(string orderType)
+        {
+            _orderType = orderType;
+        }
+        //whether it be buy or sell
+        public abstract void GetOrder(float balance);
+    }
+    public class MarketBuy : Order
+    {
+        private float _price;
+        // this is an
+        // MarketBuy(buy
+        public MarketBuy(string assetName, float price, float balance, string orderType) : base (assetName)
+        {
+            _price = price;
+            _balance = balance;
+
+        }
+        public float GetPrice()
+        {
+            return _price;
+        }
+        public void SetPrice(float price)
+        {
+            _price = price;
+        }
+        public override void GetOrder(float balance)
+        {
+            // do the function of how many you can buy at a price
+
+            // show user total balance before 
+            // show user the price of the asset and total balance of the asset
+            Console.WriteLine("Amount you'd like to buy: ");
+            float amount = float.Parse(Console.ReadLine());
+            balance = _balance - amount;
+            Console.WriteLine(balance);
+        }
+    }
+    public class MarketSell : Order
+    {
+        public MarketSell(string assetName, string orderType, int id, float balance = 143.5F) : base(assetName, orderType, id, balance)
+        {
+        }
+        public override void GetOrder(float balance)
+        {
+            // do the function of how many you can buy at a price
+
+            // show user total balance before 
+            // show user the price of the asset and total balance of the asset
+            Console.WriteLine("Amount you'd like to buy: ");
+            float amount = float.Parse(Console.ReadLine());
+            balance = _balance + amount;
+            Console.WriteLine(balance);
+        }
+    }
+    public class LimitBuy : Order
+    {
+        public LimitBuy(string assetName, string orderType, int id, float balance = 143.5F) : base(assetName, orderType, id, balance)
+        {
+        }
+        public override void GetOrder(float balance)
+        {
+            // do the function of how many you can buy at a price
+
+            // show user total balance before 
+            // show user the price of the asset and total balance of the asset
+            Console.WriteLine("Amount you'd like to buy: ");
+            float amount = float.Parse(Console.ReadLine());
+            balance = _balance - amount;
+            Console.WriteLine(balance);
+        }
+    }
+    public class LimitSell : Order
+    {
+        public LimitSell(string assetName, string orderType, int id, float balance = 143.5F) : base(assetName, orderType, id, balance)
+        {
+        }
+        public override void GetOrder(float balance)
+        {
+            // do the function of how many you can buy at a price
+
+            // show user total balance before 
+            // show user the price of the asset and total balance of the asset
+            Console.WriteLine("Amount you'd like to sell: ");
+            float amount = float.Parse(Console.ReadLine());
+            balance = _balance + amount;
+            Console.WriteLine(balance);
+        }
+
+    }
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,76 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        // You can create a list out of Shape objects
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        // we add this shape to the list
+        shapes.Add(s1);
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(s2);
+        
+        foreach (Shape s in shapes)
+        {
+            // Notice that all shapes have a GetColor method from the base class
+            string color = s.GetColor();
+
+            // Notice that all shapes have a GetArea method, but the behavior is
+            // different for each type of shape
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
+}
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string color)
+    {
+        _color=color;
+    }
+
+    // Abstract
+    public abstract double GetArea();
+}
+public class Square : Shape
+{
+    private double _side;
+    public Square(string color, double side) : base (color)
+    {
+        _side=side;
+    }
+    // override
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+
+}
+public class Rectangle : Shape
+{
+    private double _width;
+    private double _length;
+    public Rectangle(string color, double width,double length) : base (color)
+    {
+        _width = width;
+        _length = length;
+    }
+    // override
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+
 }
